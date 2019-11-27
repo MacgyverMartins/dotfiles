e89cb57ecb00446c2ac174d3a9f509395808c397
Macgyver <macgyver.martins@gmail.com>
2019-11-27 08:22:48 -0300

    update brewinstall

---
+++ brewinstall.sh
@@ -0,0 +1,10 @@
+# install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install brew dependencies
+brew cask install iterm2
+brew casck install zeplin
+brew install zsh-autosuggestions
+brew install node
+brew cask install alfred
+brew install yarn
